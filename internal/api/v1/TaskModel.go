cabce0992c8425ae70eb33e2e686ec52b25deadf
萧 <1243925457@qq.com>
2021-10-19 09:09:04 +0800

    Create TaskModel.go

---
+++ internal/api/v1/TaskModel.go
@@ -0,0 +1,44 @@
+package v1
+
+import (
+	"github.com/kamva/mgm/v3"
+	"time"
+)
+
+type TaskConfig struct {
+	mgm.DefaultModel `bson:",inline"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Label       string `json:"label"`
+	CycleType   int8   `json:"cycleType"`
+	UserName    string `json:"userName"`
+}
+
+func NewTaskConfig(name, description, label string, cycleType int8) *TaskConfig {
+	return &TaskConfig {
+		Name: name,
+		Description: description,
+		Label: label,
+		CycleType: cycleType,
+	}
+}
+
+type TaskRecord struct {
+	mgm.DefaultModel `bson:",inline"`
+	CompleteDate     time.Time        `json:"date"`
+	Name             string           `json:"name"`
+	Description      string           `json:"description"`
+	Label            string           `json:"label"`
+	CycleType        int8             `json:"cycleType"`
+	UserName         string           `json:"userName"`
+}
+
+func NewTaskRecord(name, description, label string, cycleType int8, completeDate time.Time) *TaskRecord {
+	return &TaskRecord {
+		Name: name,
+		Description: description,
+		Label: label,
+		CycleType: cycleType,
+		CompleteDate: completeDate,
+	}
+}
