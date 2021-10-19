014fdaae23ffc51b2e7dceeb18682e2a666eff47
萧 <1243925457@qq.com>
2021-10-19 12:18:42 +0800

    重构模型

---
+++ internal/api/v1/TaskConfig.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	"github.com/kamva/mgm/v3"
+)
+
+type TaskConfig struct {
+	mgm.DefaultModel `bson:",inline"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Label       string `json:"label"`
+	CycleType   int8   `json:"cycleType"`
+	UserName    string `json:"userName"`
+	IsComplete  bool   `json:"isComplete"`
+}
+
+func NewTaskConfig(name, description, label string, cycleType int8) *TaskConfig {
+	return &TaskConfig {
+		Name: name,
+		Description: description,
+		Label: label,
+		CycleType: cycleType,
+		IsComplete: false,
+	}
+}
--- internal/api/v1/TaskRecord.go
+++ internal/api/v1/TaskRecord.go
@@ -5,24 +5,6 @@ import (
 	"time"
 )
 
-type TaskConfig struct {
-	mgm.DefaultModel `bson:",inline"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Label       string `json:"label"`
-	CycleType   int8   `json:"cycleType"`
-	UserName    string `json:"userName"`
-}
-
-func NewTaskConfig(name, description, label string, cycleType int8) *TaskConfig {
-	return &TaskConfig {
-		Name: name,
-		Description: description,
-		Label: label,
-		CycleType: cycleType,
-	}
-}
-
 type TaskRecord struct {
 	mgm.DefaultModel `bson:",inline"`
 	CompleteDate     time.Time        `json:"date"`
