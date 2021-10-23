6cc03946b1114be04ef2c6baf75588c7f2146f59
萧 <1243925457@qq.com>
2021-10-23 11:11:47 +0800

    定义统一返回结构

---
+++ internal/growth_record/controller/ControllerCommon.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func ErrorResponse(c *gin.Context, code int16, message string) {
+	c.JSON(int(code), gin.H{
+		"code": code,
+		"message": message,
+	})
+}
+
+func SuccessResponse(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"data": data,
+	})
+}
