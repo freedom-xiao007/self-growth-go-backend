028888217e34296f4c811c1d8b5225e95d5416a7
萧 <1243925457@qq.com>
2021-10-30 15:18:21 +0800

    任务组新增完善

--- internal/api/v1/TaskGroup.go
+++ internal/api/v1/TaskGroup.go
@@ -8,11 +8,20 @@ type TaskGroup struct {
 	mgm.DefaultModel `bson:",inline"`
 	Name             string `json:"name"`
 	UserName         string `json:"username"`
+	Description      string `json:"description"`
+	Label            string `json:"label"`
+	CycleType        int8   `json:"cycleType"`
+	IsComplete       bool   `json:"isComplete"`
+	LearnType        int8   `json:"learnType"`
 }
 
-func NewTaskGroup(name, username string) *TaskGroup {
+func NewTaskGroup(name, username, description, label string, cycleType, learnType int8) *TaskGroup {
 	return &TaskGroup {
 		Name:        name,
 		UserName: username,
+		Description: description,
+		Label: label,
+		CycleType: cycleType,
+		LearnType: learnType,
 	}
 }
