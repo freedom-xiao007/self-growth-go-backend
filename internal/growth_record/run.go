6ec9d2f7214b53022f71dece42507806172d931c
萧 <1243925457@qq.com>
2021-11-06 13:21:02 +0800

    Update run.go

--- internal/growth_record/run.go
+++ internal/growth_record/run.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/kamva/mgm/v3"
 	log "github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"os"
 	"seltGrowth/internal/growth_record/controller"
 	"seltGrowth/internal/growth_record/middleware"
+	"seltGrowth/internal/pkg/store/mongodb"
 )
 
-func initMongodb() {
-	// Setup the mgm default config
-	username := os.Getenv("mongo_user")
-	passowrd := os.Getenv("mongo_password")
-	host := os.Getenv("mongo_host")
-	port := os.Getenv("mongo_port")
-	mongoURI := fmt.Sprintf("mongodb://%s:%s@%s:%s", username, passowrd, host, port)
-	log.Info("mongoURI", mongoURI)
-	err := mgm.SetDefaultConfig(nil, "phone_record", options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	initMongodb()
+	mongodb.InitMongodb()
 	router := gin.Default()
 	router.Use(middleware.Cors())
 	InitRoute(router)
