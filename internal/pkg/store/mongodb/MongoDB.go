271183983aa2d91889ee8c224500ead686d3b3d9
萧 <1243925457@qq.com>
2021-11-06 13:20:36 +0800

    Create MongoDB.go

---
+++ internal/pkg/store/mongodb/MongoDB.go
@@ -0,0 +1,38 @@
+package mongodb
+
+import (
+	"fmt"
+	"github.com/kamva/mgm/v3"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+)
+
+func InitMongodb() {
+	// Setup the mgm default config
+	username := os.Getenv("mongo_user")
+	password := os.Getenv("mongo_password")
+	host := os.Getenv("mongo_host")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+	port := os.Getenv("mongo_port")
+	if port == "" {
+		port = "27017"
+	}
+
+	if username == "" && password == "" {
+		err := mgm.SetDefaultConfig(nil, "phone_record", options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	mongoURI := fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+	log.Info("mongoURI", mongoURI)
+	err := mgm.SetDefaultConfig(nil, "phone_record", options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
